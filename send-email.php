9e20be63716dea4d144a96c7d556ef29805d965d
paulomilitante <paulo.militante@gmail.com>
2018-04-25 15:25:57 +0800

    added sendgrid

--- send-email.php
+++ send-email.php
@@ -1,9 +1,22 @@
 <?php 
 
+require "sendgrid-php/sendgrid-php.php";
 
 $email = $_POST['email'];
 
-mail($email,"Test Subject","Test");
+$from = new SendGrid\Email(null, "test@example.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, $email);
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
 
 header("location: index.html");
 
