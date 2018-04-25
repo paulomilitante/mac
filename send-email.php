5f5f442bb020e06bdf4a740aa40bde1b15196a81
paulomilitante <paulo.militante@gmail.com>
2018-04-25 17:47:47 +0800

    added name in form

--- send-email.php
+++ send-email.php
@@ -3,7 +3,6 @@
 require "sendgrid-php/sendgrid-php.php";
 
 $email = $_POST['email'];
-$name = $_POST['name'];
 $prodname = $_POST['prodname'];
 $proddesc = $_POST['proddesc'];
 $image = $_POST['image'];
@@ -19,7 +18,7 @@ $body = "<html>
 $from = new SendGrid\Email("MAC Cosmetics PH","macgirlsquiz@maccosmeticsph.com");
 $subject = "Your #MACGirls Quiz Result";
 $to = new SendGrid\Email(null, $email);
-$content = new SendGrid\Content("text/plain", $body);
+$content = new SendGrid\Content("text/html", $body);
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
