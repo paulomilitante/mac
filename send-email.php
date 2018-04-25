951508a9e69e1c4b0c4fd725a514b33136dd6764
paulomilitante <paulo.militante@gmail.com>
2018-04-25 17:11:51 +0800

    added sendgrid test

--- send-email.php
+++ send-email.php
@@ -3,11 +3,12 @@
 require "sendgrid-php/sendgrid-php.php";
 
 $email = $_POST['email'];
+$name = $_POST['name'];
 
 $from = new SendGrid\Email("MAC Cosmetics PH","macgirlsquiz@maccosmeticsph.com");
 $subject = "Your #MACGirls Quiz Result";
-$to = new SendGrid\Email(null, $email);
-$content = new SendGrid\Content("text/plain", "Test Emai Hello!");
+$to = new SendGrid\Email($name, $email);
+$content = new SendGrid\Content("text/plain", "Test Email Hello!");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
